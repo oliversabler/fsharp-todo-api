c85168762b643f6592f927de9176a84e62b56d9b
oliversabler <oliver.sabler@gmail.com>
2021-09-30 22:03:53 +0200

    Working GetAll, Get and Create endpoints

--- Web.Api/Program.fs
+++ Web.Api/Program.fs
@@ -1,13 +1,86 @@
-﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
-
+﻿open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.Hosting
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.AspNetCore.Http
+open Giraffe
+open Giraffe.ViewEngine
+open FSharp.Control.Tasks
+open Todos
 open System
 
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+type PingModel = {
+    Response: string
+}
+
+module Handlers =
+    let viewTaskHandler (id : Guid) = 
+        fun (next : HttpFunc) (ctx : HttpContext) ->
+            task {
+                let store = ctx.GetService<Store>()
+                let todo = store.Get(id)
+                return! json todo next ctx
+            }
+
+    let viewTasksHandler = 
+        fun (next : HttpFunc) (ctx : HttpContext) ->
+            task {
+                let store = ctx.GetService<Store>()
+                let todos = store.GetAll()
+                return! json todos next ctx
+            }
+
+    let createTaskHandler =
+        fun (next : HttpFunc) (ctx : HttpContext) ->
+            task {
+                let! newTodo = ctx.BindJsonAsync<NewTodo>()
+                let store = ctx.GetService<Store>()
+                let created = store.Create({ 
+                    Id = Guid.NewGuid(); 
+                    Description = newTodo.Description; 
+                    Created = DateTime.UtcNow; 
+                    IsCompleted = false 
+                })
+                return! json created next ctx
+            }
+
+let apiTodoRoutes : HttpHandler =
+    subRoute "/todo/"
+        (choose [
+            GET >=> choose [
+                routef "/%O" Handlers.viewTaskHandler
+                route "" >=> Handlers.viewTasksHandler
+            ]
+            POST >=> route "" >=> Handlers.createTaskHandler
+        ])
+
+let webApp =
+    choose [
+        route "/ping"   >=> text "pong"
+        GET >=> route "/"
+        subRoute "/api"
+            (choose [
+                apiTodoRoutes
+                GET >=> route "" >=> json { Response = "Todo List API" }
+            ])
+        //setStatusCode 404 >=> text "Not Found"
+    ]
+
+let configureApp (app : IApplicationBuilder) =
+    app.UseGiraffe webApp
+
+let configureServices (services : IServiceCollection) =
+    services.AddGiraffe()
+            .AddSingleton<Store>(Store()) |> ignore
 
 [<EntryPoint>]
-let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
-    0 // return an integer exit code
+let main _ =
+    Host.CreateDefaultBuilder()
+        .ConfigureWebHostDefaults(fun webHost ->
+            webHost
+                .Configure(configureApp)
+                .ConfigureServices(configureServices)
+                |> ignore)
+        .Build()
+        .Run()
+    0
