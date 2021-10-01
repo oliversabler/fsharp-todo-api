f1bf30b7bbb8d61cf408443678796311c9a33c00
oliversabler <oliver.sabler@gmail.com>
2021-10-01 20:21:17 +0200

    Move handlers to separate file

---
+++ Web.Api/Handlers.fs
@@ -0,0 +1,55 @@
+﻿module Handlers
+
+open System
+open Microsoft.AspNetCore.Http
+open Giraffe
+open Todos
+open System.Collections.Generic
+
+let getTaskHandler (id : Guid) = 
+    fun (next : HttpFunc) (ctx : HttpContext) ->
+        task {
+            let store = ctx.GetService<Store>()
+            let todo = store.Get(id)
+            return! json todo next ctx
+        }
+
+let getTasksHandler = 
+    fun (next : HttpFunc) (ctx : HttpContext) ->
+        task {
+            let store = ctx.GetService<Store>()
+            let todos = store.GetAll()
+            return! json todos next ctx
+        }
+
+let createTaskHandler =
+    fun (next : HttpFunc) (ctx : HttpContext) ->
+        task {
+            let! newTodo = ctx.BindJsonAsync<NewTodo>()
+            let store = ctx.GetService<Store>()
+            let created = store.Create({ 
+                Id = Guid.NewGuid(); 
+                Description = newTodo.Description; 
+                Created = DateTime.UtcNow; 
+                IsCompleted = false 
+            })
+            return! json created next ctx
+        }
+
+let updateTaskHandler =
+    fun (next : HttpFunc) (ctx : HttpContext) ->
+        task {
+            let! todo = ctx.BindJsonAsync<Todo>()
+            let store = ctx.GetService<Store>()
+            let updated = store.Update(todo)
+            return! json updated next ctx
+        }
+
+let deleteTaskHandler (id : Guid) = 
+    fun (next : HttpFunc) (ctx : HttpContext) ->
+        task {
+            let store = ctx.GetService<Store>()
+            let existing = store.Get(id)
+            let deleted = store.Delete(KeyValuePair<TodoId, Todo>(id, existing))
+            return! json deleted next ctx
+        }
--- Web.Api/Program.fs
+++ Web.Api/Program.fs
@@ -2,81 +2,25 @@
 open Microsoft.AspNetCore.Hosting
 open Microsoft.Extensions.Hosting
 open Microsoft.Extensions.DependencyInjection
-open Microsoft.AspNetCore.Http
 open Giraffe
-open FSharp.Control.Tasks
 open Todos
 open System
-open System.Collections.Generic
 open Microsoft.Extensions.Logging
-
-//////////////
-// Handlers //
-//////////////
-
-let errorHandler (ex: Exception) (logger: ILogger) =
-    logger.LogError(EventId(), ex, "An unhandled exception has occurred while executing the request.")
-    clearResponse >=> setStatusCode 500 >=> text ex.Message
-
-module Handlers =
-    let viewTaskHandler (id : Guid) = 
-        fun (next : HttpFunc) (ctx : HttpContext) ->
-            task {
-                let store = ctx.GetService<Store>()
-                let todo = store.Get(id)
-                return! json todo next ctx
-            }
-
-    let viewTasksHandler = 
-        fun (next : HttpFunc) (ctx : HttpContext) ->
-            task {
-                let store = ctx.GetService<Store>()
-                let todos = store.GetAll()
-                return! json todos next ctx
-            }
-
-    let createTaskHandler =
-        fun (next : HttpFunc) (ctx : HttpContext) ->
-            task {
-                let! newTodo = ctx.BindJsonAsync<NewTodo>()
-                let store = ctx.GetService<Store>()
-                let created = store.Create({ 
-                    Id = Guid.NewGuid(); 
-                    Description = newTodo.Description; 
-                    Created = DateTime.UtcNow; 
-                    IsCompleted = false 
-                })
-                return! json created next ctx
-            }
-
-    let updateTaskHandler =
-        fun (next : HttpFunc) (ctx : HttpContext) ->
-            task {
-                let! todo = ctx.BindJsonAsync<Todo>()
-                let store = ctx.GetService<Store>()
-                let updated = store.Update(todo)
-                return! json updated next ctx
-            }
-
-    let deleteTaskHandler (id : Guid) = 
-        fun (next : HttpFunc) (ctx : HttpContext) ->
-            task {
-                let store = ctx.GetService<Store>()
-                let existing = store.Get(id)
-                let deleted = store.Delete(KeyValuePair<TodoId, Todo>(id, existing))
-                return! json deleted next ctx
-            }
 
 /////////////
 // Web App //
 /////////////
 
+let errorHandler (ex: Exception) (logger: ILogger) =
+    logger.LogError(EventId(), ex, "An unhandled exception has occurred while executing the request.")
+    clearResponse >=> setStatusCode 500 >=> text ex.Message
+
 let apiTodoRoutes : HttpHandler =
     subRoute "/todo/"
         (choose [
             GET >=> choose [
-                routef "%O" Handlers.viewTaskHandler
-                route "" >=> Handlers.viewTasksHandler
+                routef "%O" Handlers.getTaskHandler
+                route "" >=> Handlers.getTasksHandler
             ]
             POST >=> route "" >=> Handlers.createTaskHandler
             PUT >=> route "" >=> Handlers.updateTaskHandler
